09bbe42eca9e217230d5c96812d1809c69ebdb18
Ekky Sukmawati <ekkys99@gmail.com>
2024-05-19 20:49:12 +0700

    Add ORM

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function applicants()
+    {
+        return $this->hasMany(ProjectApplicant::class);
+        // menampilkan kandiddat yg mau mengerjakan project
+    }
+
+    public function tools()
+    {
+        return $this->belongsToMany(Tool::class, 'project_tools', 'project_id', 'tool_id')
+            ->wherePivotNull('delete_at')
+            ->withPivot('id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,41 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+        //satu user satu wallet
+    }
+
+    public function walletTransaction()
+    {
+        //satu user banyak transaksi
+        return $this->hasMany(WalletTransaction::class);
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'client_id', 'id')->orderByDesc('id');
+        //satu user banyak project
+        //client_id perlu di definisikan sebab aslinya user_id
+        // cara akses $user->projects() .... menampilkan seluruh project dari user tersebut
+        // dibuat supaya ketika menampilkan data tidak perlu query di viewnya sehingga lebih ringan
+    }
+
+    public function proposals()
+    {
+        return $this->hasMany(ProjectApplicant::class, 'freelancer_id', 'id');
+        // untuk menampilkan user ini sudah apply project apa saja?
+    }
+
+    public function hasAppliedProject($projectId)
+    {
+        return ProjectApplicant::where('project_id', $projectId)
+            ->where('freelancer_id', $this->id)
+            ->exists(); // true or flase
+        //satu freelancer hanya applied 1 project dalam 1 waktu,
+        //jika selesai baru bisa applied lagi
+        // id nya di ambilkan pas login
+    }
 }
